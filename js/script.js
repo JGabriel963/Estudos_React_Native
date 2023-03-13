3889cdd48dd9ef875091d4ffaa8102387d9ed795
João Gabriel <joaogabriel9633@gmail.com>
2023-03-13 00:46:19 -0300

    Add audio

--- js/script.js
+++ js/script.js
@@ -1,14 +1,18 @@
 const btnGerador = document.getElementById('generate');
 const ispinIcon = document.querySelector('.spinner')
-const res = document.getElementById('result')
+const res = document.getElementById('result');
+const audio = document.querySelector('audio')
 
 btnGerador.addEventListener('click', function () {
+    audio.play()
+    audio.currentTime = 0.1
     res.classList.add('checked');
     ispinIcon.classList.add('spin')
     document.querySelector('#result > span').textContent = "Carregando...";
 
 
     setTimeout(() => {
+        audio.pause()
         res.classList.remove('checked');
         const min = parseInt(document.getElementById('min').value)
         const max = parseInt(document.getElementById('max').value)
