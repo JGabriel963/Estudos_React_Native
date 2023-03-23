eb48491d732ca89cd4051442dbc47865b570a710
João Gabriel <joaogabriel9633@gmail.com>
2023-03-23 17:09:53 -0300

    Add backgrounAudio

--- js/script.js
+++ js/script.js
@@ -1,10 +1,14 @@
 const btnGerador = document.getElementById('generate');
 const ispinIcon = document.querySelector('.spinner')
 const res = document.getElementById('result');
-const audio = document.querySelector('audio')
+const audio = document.getElementById('audio');
+const audioFundo = document.getElementById('audio-fundo');
+const audioFundoBtn = document.getElementById('fundo-btn');
+let statusBackgroundMusic = false
 
 btnGerador.addEventListener('click', function () {
     audio.play()
+    audioFundo.volume = 0.4
     audio.currentTime = 0.1
     res.classList.add('checked');
     ispinIcon.classList.add('spin')
@@ -13,6 +17,7 @@ btnGerador.addEventListener('click', function () {
 
     setTimeout(() => {
         audio.pause()
+        audioFundo.volume = 1
         res.classList.remove('checked');
         const min = parseInt(document.getElementById('min').value)
         const max = parseInt(document.getElementById('max').value)
@@ -27,3 +32,18 @@ btnGerador.addEventListener('click', function () {
     }, 5000)
  
 })
+
+function backgroundAudio() {
+    if (statusBackgroundMusic === false) {
+        audioFundo.play()
+        audioFundo.currentTime = 14
+        audioFundoBtn.innerHTML = `<i class="fa-solid fa-pause"> </i>`  
+        statusBackgroundMusic = true
+    } else {
+        audioFundo.pause()
+        audioFundoBtn.innerHTML = `<i class="fa-solid fa-play"></i>`
+        statusBackgroundMusic = false
+    }
+}
+
+audioFundoBtn.addEventListener('click', backgroundAudio)
